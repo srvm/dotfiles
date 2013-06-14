1a3244427a7a1e57a124db88ecbb48de84e6e7c9
Saurav Muralidharan <sauravm@nvidia.com>
2013-06-13 17:56:05 -0700

    Initial version of VIM setup

---
+++ setup_vim_plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please install Pathogen first!"
+echo "Oh and make sure git is installed too"
+read -p "Press [Enter] when ready..."
+
+cd ~/.vim/bundle
+
+# git clone git@github.com:kien/rainbow_parentheses.vim.git
+# git clone git@github.com:mileszs/ack.vim.git
+
+# and so on...
